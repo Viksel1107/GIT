d63aee52b7d09ea12c76785e50274ce778d3f494
Vitalii Leskiv <viksel1107@gmail.com>
2023-11-19 14:49:59 +0100

    add for

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 int main() {
     std::cout << "Hello!" << std::endl;
     std::cout << "World!" << std::endl;
+    for (int i = 0; i < 10; ++i) {
+        std::cout << "Hello World!" << std::endl;
+    }
     return 0;
 }
