5c40e922e7cd422617a42bde8c1489212308681f
Vitalii Leskiv <viksel1107@gmail.com>
2023-11-19 14:49:19 +0100

    add world

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello!" << std::endl;
+    std::cout << "World!" << std::endl;
     return 0;
 }
