de25e8c62e3911a2839c9caae9a2b75cdc7a519a
Vitalii Leskiv <viksel1107@gmail.com>
2023-11-19 14:37:36 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GIT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GIT.dir/main.cpp.o"
+  "GIT"
+  "GIT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GIT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
